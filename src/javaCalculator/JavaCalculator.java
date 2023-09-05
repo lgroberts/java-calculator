b1b29708d5b6f00529ceeee1ba9ba12811725cf7
LILIANA ROBERTS <LGROBERT@ASU.EDU>
2023-09-04 18:10:46 -0700

    addition of edited files

--- src/javaCalculator/JavaCalculator.java
+++ src/javaCalculator/JavaCalculator.java
@@ -1,6 +1,22 @@
 package javaCalculator;
 
 public class JavaCalculator {
+	
+	public Integer add(int x, int y) {
+		return x + y;
+	}
+	public Integer subtraction(int x, int y) {
+		return x - y;
+	}
+	public Integer multiplication(int x, int y) {
+		return x * y;
+	}
+	public Integer division(int x, int y) {
+		return x / y;
+	}
+	public Integer square(int x, int y) {
+		return x * x;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/javaCalculator/MagicCalculator.java
+++ src/javaCalculator/MagicCalculator.java
@@ -1,5 +1,24 @@
 package javaCalculator;
 
-public class MagicCalculator {
+public class MagicCalculator extends JavaCalculator {
 
+	public Double squareRoot(int x, int y) {
+		return Math.sqrt(x);
+	}
+	public Double sine(int x, int y) {
+		return Math.sin(x);
+	}
+	public Double cosine(int x, int y) {
+		return Math.cos(x);
+	}
+	public Double tangent(int x, int y) {
+		return Math.tan(x);
+	}
+	public Double factorial(int x, int y) {
+		double factorial = 1;
+		for (int i = 1; i <= x; i++) {
+			factorial *= i;
+		}
+		return factorial;
+	}
 }
